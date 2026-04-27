6fedfb44617472ed05434184c4014e2edfa32906
fkmccar01 <fkmccar01@gmail.com>
2026-04-27 15:03:40 -0400

    Create tweets.py

---
+++ tweets.py
@@ -0,0 +1,12 @@
+TWEETS = [
+    "We’ll know more in the coming days, but tonight I am thankful, as we all should be, for the quick and incredibly brave actions of law enforcement, and I am grateful that everyone is safe. Violence of any kind is abhorrent and should be universally condemned.",
+    "Trump is unhinged and this insane rhetoric is undermining and creating panic. STFU and end it.",
+    "Tens of thousands of children from Ukraine have been stolen by Putin. We sent a bipartisan letter urging the Administration to hold Russian oil companies – benefiting from Trump waived sanctions – accountable for their direct involvement in these abductions.",
+    "One of the most remarkable demonstrations of political cowardice and cruelty. The Speaker caved to the extremists in his party, denying paychecks and funding for all the law-abiding parts of Homeland Security.",
+    "Most, if not all nations with nuclear weapons, are unlikely to use them. The regime in Iran would. This is the catastrophic nightmare we are trying to avoid.",
+    "Where Trump and Hegseth have failed, our uniformed service members have succeeded. While the Administration failed to communicate and prepare the country for this operation, General Kaine and Admiral Cooper have demonstrated extraordinary leadership, and I appreciate them and every one of the sailors, soldiers, and airmen – especially those that gave their lives fighting against this incredibly dangerous regime.",
+    "For most of my life, the regime and Iran has been a national security threat. The amount of enriched uranium for a nuclear bomb, which they brag about having, is arguably the most serious national and global security threat of our time.",
+    "Attacking a place where people go to worship is evil, and it has to stop. We’re praying for everyone at Temple Israel and are grateful for the security guard and first responders who acted so quickly to keep people safe.",
+    "Your tariffs have made nearly everything more expensive. Healthcare costs have skyrocketed. Your economy is a disaster for most people.",
+    "Noem was a disaster. Reckless in almost every way. Now Trump must rein in ICE and end its chaos."
+]
